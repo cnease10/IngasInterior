161a38674570286204deaa737ce6d5191a1ea793
cnease10 <cierra.nease10@gmail.com>
2020-10-03 15:27:01 -0700

    nav component up and linked to header component

--- ingasinterior/src/Header/index.js
+++ ingasinterior/src/Header/index.js
@@ -1,11 +1,12 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-
+import Nav from '../Nav'
 function Header() {
 
      return (
          <React.Fragment>
              <h1>Inga's Interior</h1>
+             < Nav />
          </React.Fragment>
      )
  
---
+++ ingasinterior/src/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+    return(
+        <React.Fragment>
+            <p>I'm a nav</p>
+        </React.Fragment>
+    )
+}
+
+export default Nav
